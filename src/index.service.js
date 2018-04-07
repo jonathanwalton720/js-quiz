9d8c596d3a0668c5821b1124678791c78bc94d21
jonathanwalton720 <jonathanwalton720@gmail.com>
2018-04-06 22:18:37 -0700

    adding html webpack plugin and moving some things around

---
+++ src/index.service.js
@@ -0,0 +1,24 @@
+
+var allQuestions = [
+  { question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer: 0 },
+  { question: "If a snail climbed up a 12 ft wall at a steady rate of 3 ft per day, but slipped down 2 ft every night, how many days would it take him to reach the top?", choices: ["10 days", "12 days", "4 days", "8 days"], correctAnswer: 0 },
+  { question: "On his egg deliveries, farmer Fred sets off with 400 eggs. At the first delivery he drops off 5 doz`, at the second 12 doz`, and at the last one he delivers 15 doz`. How many eggs does he take home to his brooding wife?", choices: ["Sixteen", "Twelve", "Four", "Fifteen"], correctAnswer: 0 },
+  { question: "According to the song, from which track does the Chattanooga choo choo leave Pennsylvania station?", choices: ["Track 29", "Track 37", "Track 22", "Track 13"], correctAnswer: 0 },
+  { question: "Who do Vince and Musky accompany in their cartoon capers?", choices: ["Deputy Dawg", "Mighty Mouse", "Dinky Duck"], correctAnswer: 0 },
+  { question: "What girls name is also the term used to describe a female donkey?", choices: ["Jenny", "Sow", "Vixen", "Mare"], correctAnswer: 0 },
+  { question: "Name the king who failed to keep an eye on things at the battle of Hastings?", choices: ["Harold", "Alfred", "Edward", "Cathute"], correctAnswer: 0 },
+  { question: "Traditionally, what type of wood do Rolls Royce use on the dashboards of their cars?", choices: ["Walnut", "Mahogany", "Oak", "Teak"], correctAnswer: 0 },
+  { question: "In which sport would you use a chucker?", choices: ["Polo", "Cricket", "Golf"], correctAnswer: 0 },
+  { question: "If you happened to be plaining and pearling what would you be doing?", choices: ["Knitting", "Shucking Oysters", "Surfing", "Skydiving"], correctAnswer: 0 },
+  { question: "What is the national emblem of Canada?", choices: ["Maple Leaf", "Sycamore Leaf", "Fig Leaf"], correctAnswer: 0 },
+  { question: "Are Gorillas carnivores, omnivores, or herbivores?", choices: ["Carnivores", "Omnivores", "Herbivores"], correctAnswer: 2 },
+  { question: "Free Willey was a film about what?", choices: ["A whale", "A duck", "A labrador"], correctAnswer: 0 },
+  { question: "Can pigs swim? Yes or no.", choices: ["Yes", "No"], correctAnswer: 0 },
+  { question: "What World War II operation was code named `Dynamo`?", choices: ["Evacuation of Dunkirk", "First Arctic Convoy to the Soviet Union", "Anti-shipping operation off Norway"], correctAnswer: 0 },
+  { question: "How many old pennies were there in a Groat?", choices: ["Four", "Twenty-Five", "Ten"], correctAnswer: 0 },
+  { question: "What would a farmer do with his mangelwurzel?", choices: ["Feed it to his animals", "Mix it with his fertilizer"], correctAnswer: 0 },
+  { question: "In the song `The 12 days of Christmas`, what are the 6 geese a-doing?", choices: ["Laying", "Leaping", "Honking"], correctAnswer: 0 },
+  { question: "In cricket, where would you find the chain?", choices: ["Between the wickets.", "Surrounding the boundary.", "Measuring the pitch."], correctAnswer: 0 },
+];
+
+module.exports = allQuestions;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
-var packageJson = require('../package.json');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var packageJson = require('./package.json');
 
 module.exports = {
   entry: {
-    app: '.src/index.js',
+    app: './src/index.js',
     vendor: Object.keys(packageJson.dependencies)
   },
   output: {
-    filename: "./dist/[name].js"
+    filename: "[name].bundle.js"
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({template: './src/index.html'})]
 }
